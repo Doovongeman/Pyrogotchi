0205978d1fc0fdc05cbb0207b218df157401015b
wyel <wyel88@gmail.com>
2016-05-06 22:20:58 -0400

    Basic fire VS burnable object system going on

--- Pyrogotchi/Assets/Script/Fire.cs
+++ Pyrogotchi/Assets/Script/Fire.cs
@@ -3,13 +3,53 @@ using System.Collections;
 
 public class Fire : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+	public float decayRate = -0.1f;
+
+
+
+
+	void Start()
+	{
+		StartCoroutine (UpdateDecayRate());
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+
+
+	void Update()
+	{
+		AdjustSize ();
 	}
+
+
+	IEnumerator UpdateDecayRate()
+	{
+		while(true) 
+		{ 
+			//AdjustSize ();
+			yield return new WaitForSeconds(1);
+		}
+	}
+
+
+
+	public void StartGrowing(float contribution)
+	{
+		decayRate += contribution;
+	}
+
+
+	public void StopGrowing(float contribution)
+	{
+		decayRate -= contribution;
+	}
+
+
+	private void AdjustSize()
+	{
+		print (decayRate);
+	}
+
+
+
+
 }

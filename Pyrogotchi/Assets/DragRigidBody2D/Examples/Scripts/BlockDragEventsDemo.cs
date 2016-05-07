d1ed45c37816cdbb48ca7c5f866c61772641b7b5
Doovongeman <john.axon@yahoo.ca>
2016-05-06 22:46:02 -0400

    Added a scene called Test where you can have objects that drag over other objects and snapback to their original position. Added a prefab called BurnableObject that has these properties. Added a little SnapBack script. Modified the BlockDragEvents.cs script.

---
+++ Pyrogotchi/Assets/DragRigidBody2D/Examples/Scripts/BlockDragEventsDemo.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlockDragEventsDemo : MonoBehaviour {
+
+    public Color dragColor;
+    public SpriteRenderer dragIndicatorRenderer;
+    public TextMesh dragTimeIndicatorTextMesh;
+
+    protected bool isDragging = false;
+    protected Color originalIndicatorColor;
+
+	private Rigidbody2D rb2d;
+	private SnapBack sb;
+
+	void Start() {
+		//I set the kinematic thing to true by default so that pulling an object over 
+		//another object doesn't make them collide with each other
+		rb2d = transform.GetComponent<Rigidbody2D> ();
+		rb2d.isKinematic = true;
+		sb = transform.GetComponent<SnapBack> ();
+	}
+
+    //This event is called when you first pick up this GameObject
+	void OnStartDrag()
+    {
+        isDragging = true;
+
+        dragTimeIndicatorTextMesh.text = "0";
+
+        originalIndicatorColor = gameObject.GetComponent<SpriteRenderer>().color;
+        dragIndicatorRenderer.color = dragColor;
+
+		//Make Kinematic?
+		rb2d.isKinematic = false;
+    }
+
+    //This event is called when you drop this GameObject
+    void OnStopDrag()
+    {
+        isDragging = false;
+
+        dragTimeIndicatorTextMesh.text = "0";
+
+        dragIndicatorRenderer.color = originalIndicatorColor;
+
+		//Make Kinematic?
+		rb2d.isKinematic = true;
+		transform.position = sb.startPosition;
+    }
+
+    //You can use a bool to indicate whether the GameObject is currently being dragged.
+    void Update()
+    {
+        if(isDragging)
+        {
+            //For example, here we are updating a clock in the scene to show drag time
+            float currentTime = float.Parse(dragTimeIndicatorTextMesh.text);
+            dragTimeIndicatorTextMesh.text = (currentTime + Time.deltaTime).ToString();
+        }
+    }
+}
---
+++ Pyrogotchi/Assets/Scripts/SnapBack.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SnapBack : MonoBehaviour {
+	public Vector3 startPosition;
+
+	// Use this for initialization
+	void Start () {
+		startPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

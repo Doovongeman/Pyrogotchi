542c41c20b9b90bd3ecd95c70876bba1a05a65b6
wyel <wyel88@gmail.com>
2016-05-08 16:03:24 -0400

    tweaks on the happiness and health bar + score points

--- Pyrogotchi/Assets/Script/HappinessBar.cs
+++ Pyrogotchi/Assets/Script/HappinessBar.cs
@@ -1,16 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
 using DG.Tweening;
+using UnityEngine.UI;
 
 public class HappinessBar : MonoBehaviour {
 
+	public float score;
 
 	private GameObject happinessfilling;
 	private GameObject face0;
 	private GameObject face1;
 	private GameObject face2;
 	private GameObject face3;
-
+	private GameObject scoreText;
 
 
 
@@ -20,6 +22,8 @@ public class HappinessBar : MonoBehaviour {
 		face1 = GameObject.Find ("happy1");
 		face2 = GameObject.Find ("happy2");
 		face3 = GameObject.Find ("happy3");
+		scoreText = GameObject.Find ("CurrentScore");
+		score = 0;
 	}
 
 
@@ -31,7 +35,11 @@ public class HappinessBar : MonoBehaviour {
 		float range = max - min;
 		float progress = fireSize - min;
 		float percentage = progress / range;
+		percentage = Mathf.Clamp (percentage, 0, 1);
 		happinessfilling.transform.DOScaleX (Mathf.Clamp (percentage, 0, 1), 0.45f);
+
+		score += Mathf.RoundToInt(percentage * 100);
+		scoreText.GetComponent<Text> ().text = score+"";
 
 		if (percentage < 0.30) {
 			face0.gameObject.SetActive (true);
